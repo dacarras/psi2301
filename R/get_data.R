ee320828c8e0db032bf0c631290ae19dea6a395a
dacarras <dacarras@gmail.com>
2021-02-15 22:51:50 -0300

    update get_data function

---
+++ R/get_data.R
@@ -0,0 +1,44 @@
+#' get_data() Genera una muestra aleatoria de datos, única para el usuario, reproducible y condicional a su id númerica
+#'
+#' @param data es una tabla de datos tipo data frame, donde las filas son observaciones, y las columnas son atributos de las observaciones
+#'
+#' @param id_num es un elemento númerico, por ejemplo los 8 primeros digitos de un rut (e.g. 12128485)
+#'
+#' @param n es el tamaño de la muestra generada
+#'
+#' @return devuelve una base de datos en formato tibble
+#'
+#' @examples
+#'
+#'
+#' data_tarea_1 <- psi2301::get_data(
+#'                 data = data_iccs,
+#'                 id_num = 12128485,
+#'                 n = 500)
+#'
+#'
+#' @export
+get_data <- function(data, id_num, n){
+  # requires
+  require(dplyr)
+  require(tibble)
+
+  # remove warnings
+  options(warn=-1)
+
+  # arguments
+  data_to_be_sample <- data
+  sample_size <- n
+
+  # data frame type
+  data_to_be_sample <- tibble::as_tibble(data_to_be_sample)
+
+  # core function
+  set.seed(id_num)
+  sample_data <- dplyr::sample_n(data_to_be_sample, n = sample_size)
+  return(sample_data)
+
+  # re-establish warnings
+  options(warn=0)
+
+}
