097e1e64f1f2f35a96655747921516d21f80a7fa
dacarras <dacarras@gmail.com>
2021-02-15 23:26:07 -0300

    add iccs 2016 data

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' ICCS 2016
+#'
+#' Datos del Estudio Internacional de Educación Cívica ICCS 2016,
+#' del modulo latinoamericano.
+#'
+#' @docType data
+#'
+#' @usage data(iccs_2016_lat)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#'
+#' @examples
+#' data(iccs_16_lat)
+#' dplyr::glimpse(iccs_16_lat)
+"iccs_16_lat"
