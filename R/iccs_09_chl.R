1388f1706b7bb747f951889bd5ab8751833ed003
dacarras <dacarras@uc.cl>
2021-03-16 02:35:20 -0300

    update data files

---
+++ R/iccs_09_chl.R
@@ -0,0 +1,22 @@
+#' ICCS 2009 chl
+#'
+#' Datos del Estudio Internacional sobre Educación Cívica y Ciudadana (ICCS).
+#' Esta tabla de datos, incluye las observaciones del módulo latino american
+#' de Chile de 2009.
+#'
+#' @docType data
+#'
+#' @usage data(iccs_09_chl)
+#'
+#' @format Objeto clase \code{"cross"}; ver \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references
+#'
+#' @source \href{https://www.iea.nl/index.php/data-tools/repository/iccs}
+#'
+#' @examples
+#' data(iccs_09_chl)
+#' dplyr::glimpse((iccs_09_chl)
+"iccs_09_chl"
--- R/iccs_16_lat.R
+++ R/iccs_16_lat.R
@@ -1,4 +1,4 @@
-#' ICCS 2016 lat y ICCS 2009 chl
+#' ICCS 2016 lat
 #'
 #' Datos del Estudio Internacional sobre Educación Cívica y Ciudadana (ICCS).
 #' Esta tabla de datos, incluye las observaciones del módulo latino americano.
@@ -7,7 +7,7 @@
 #'
 #' @docType data
 #'
-#' @usage data(iccs_16_lat), o data(iccs_09_chl)
+#' @usage data(iccs_16_lat)
 #'
 #' @format Objeto clase \code{"cross"}; ver \code{\link[qtl]{read.cross}}.
 #'
@@ -21,4 +21,3 @@
 #' data(iccs_16_lat)
 #' dplyr::glimpse(iccs_16_lat)
 "iccs_16_lat"
-"iccs_09_chl"
