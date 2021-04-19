861a574e30167469e8574b7b7602f2c719f64ade
dacarras <dacarras@gmail.com>
2021-04-19 02:37:11 -0400

    update amistad intergrupal and other data

---
+++ R/amistad_intergupal.R
@@ -0,0 +1,23 @@
+#' Amistad Integrupal
+#'
+#' Muestra de 300 estudiantes de educación Media de Santiago y Temuco.
+#' Estudio busca explorar la influencia del contacto intergrupal en 
+#' la promoción de mejores actitudes hacia las minorías étnicas, 
+#' en contextos escolares.
+#' 
+#' @docType data
+#'
+#' @usage data(amistad_intergrupal)
+#'
+#' @format Objeto clase \code{"cross"}; ver \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references REFERENCIAS informacion por confirmar (20210419)
+#'
+#' @source FONDECYT informacion por confirmar (20210419)
+#'
+#' @examples
+#' data(amistad_intergrupal)
+#' dplyr::glimpse((amistad_intergrupal)
+"amistad_intergrupal"
