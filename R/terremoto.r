839d43cfcb8bfa42ccc572eed09e217aaf8f05ea
dacarras <dacarras@gmail.com>
2021-04-20 23:18:00 -0400

    add terremoto data

---
+++ R/terremoto.r
@@ -0,0 +1,29 @@
+#' Terremoto
+#'
+#' Esta base contiene algunas variables que fueron medidas en una muestra de
+#' adolescentes después del terremoto del año 2010,
+#' que a continuación se describen:
+#'
+#' mujer: 0= hombre, 1=mujer
+#' edad: años
+#' tept: diagnóstico de estrés postraumático, 0=ausencia, 1=presencia
+#' autoef: escala de sentido de eficacia personal, mínimo=10, máximo= 35
+#'
+#' Nota: datos empleados en taller 1, durante 2021.
+#'
+#' @docType data
+#'
+#' @usage data(terremoto)
+#'
+#' @format Objeto clase \code{"cross"}; ver \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references sin referencias
+#'
+#' @source sin referencias
+#'
+#' @examples
+#' data(terremoto)
+#' dplyr::glimpse(terremoto)
+"terremoto"
