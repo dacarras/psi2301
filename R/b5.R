487b8995affae6a4a678dee44bffcac31e04388b
dacarras <dacarras@gmail.com>
2021-06-22 04:14:12 -0400

    update data

---
+++ R/b5.R
@@ -0,0 +1,22 @@
+#' Inventario de personalidad B5
+#'
+#' Muestra de 151 personas, que respuestas a 60 items del big five
+#' (afabilidad, apertura, extroversion, meticulosidad, neuroticismo).
+#'
+#' @docType data
+#'
+#' @usage data(b5)
+#'
+#' @format Objeto clase \code{"cross"}; ver \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Benet-Martínez, V., & John, O. P. (1998). Los Cinco Grandes Across Cultures and Ethnic Groups: Multitrait Multimethod Analyses of the Big Five in Spanish and English. Journal of Personality and Social Psychology, 75(3), 729–750. https://doi.org/10.1037/0022-3514.75.3.729
+#'
+#'
+#' @source FONDECYT informacion por confirmar (20210419)
+#'
+#' @examples
+#' data(b5)
+#' dplyr::glimpse(b5)
+"b5"
