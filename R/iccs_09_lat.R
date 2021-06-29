de5f7c68d7f095b8fcfba9ab8f89d1988fd9feda
dacarras <dacarras@gmail.com>
2021-06-29 02:08:05 -0400

    add iccs 2009 lat

---
+++ R/iccs_09_lat.R
@@ -0,0 +1,23 @@
+#' ICCS 2009 lat
+#'
+#' Datos del Estudio Internacional sobre Educación Cívica y Ciudadana (ICCS).
+#' Esta tabla de datos, incluye las observaciones del módulo latino americano.
+#' Estos datos inlcuyen muestras representativas de estudiantes de octavo grado,
+#' de Chile, Colombia, México, República Dominicana, Guatemala, Mexico y Paraguay
+#'
+#' @docType data
+#'
+#' @usage data(iccs_09_lat)
+#'
+#' @format Objeto clase \code{"cross"}; ver \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Schulz, W., Ainley, J., & Fraillon, J. (2011). ICCS 2009 Technical Report (W. Schulz, J. Ainley, & J. Fraillon (eds.)). International Association for the Evaluation of Educational Achievement (IEA). http://www.iea.nl/fileadmin/user_upload/Publications/Electronic_versions/ICCS_2009_Technical_Report.pdf
+#'
+#' @source IEA, <https://www.iea.nl/index.php/data-tools/repository/iccs>
+#'
+#' @examples
+#' data(iccs_09_lat)
+#' dplyr::glimpse(iccs_09_lat)
+"iccs_09_lat"
