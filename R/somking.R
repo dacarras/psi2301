a5423a2a84f849fe063e8346b50b7ad92bdbcec2
dacarras <dacarras@gmail.com>
2021-07-06 02:29:04 -0400

    update data smoking

---
+++ R/somking.R
@@ -0,0 +1,24 @@
+#' Smoking on birth outcomes
+#'
+#' Estudio panel con el peso de recien nacidos, y antecedentes de las madres,
+#' incluyendo edad al nacer, consumo de cigarrillos por parte de la madre,
+#' educación de la madres, entre otros indicadores.
+#'
+#' Estos datos son empleados en Rabe-Hesketh & Skrondal (2012).
+#'
+#' @docType data
+#'
+#' @usage data(smoking)
+#'
+#' @format Objeto clase \code{"cross"}; ver \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Abrevaya, J. 2006. Estimating the eﬀect of smoking on birth outcomes using a matched panel data approach. Journal of Applied Econometrics 21: 489–519.
+#'
+#' @source https://www.stata-press.com/data/mlmus3.html
+#'
+#' @examples
+#' data(smoking)
+#' dplyr::glimpse((smoking)
+"smoking"
