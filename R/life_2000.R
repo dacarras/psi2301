ae79b11118c864d9e0a0648fabb26f6fcb063836
dacarras <dacarras@gmail.com>
2022-03-15 08:47:02 -0300

    update data objects

---
+++ R/life_2000.R
@@ -0,0 +1,26 @@
+
+#' life_2000
+#'
+#' Datos de expectativa de vida al nacer de 2000, y 
+#' producto interno bruto per carpita 'gross domestic produc)'.
+#' Datos son obtenidos de https://data.worldbank.org/
+#' 
+#' @docType data
+#'
+#' @usage data(life_2000)
+#'
+#' @format Objeto clase \code{"cross"}; ver \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Mughal, U., Carrasco, D., Brown, R., & Ayers, S. (2015). 
+#' Rehabilitating civilian victims of war through psychosocial 
+#' intervention in Sierra Leone. Journal of Applied Social Psychology, 
+#' 45(11), 593–601. https://doi.org/10.1111/jasp.12322
+#'
+#' @source http://doi.wiley.com/10.1111/jasp.12322
+#'
+#' @examples
+#' data(life_2000)
+#' dplyr::glimpse((life_2000)
+"life_2000"
